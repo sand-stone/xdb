59b3b7739625186e6f639e1ebfc193d9d920707c
Sand Stone <sand.m.stone@gmail.com>
2016-09-01 18:09:23 -0700

    gdb

--- src/main/java/xdb/GremlinDB.java
+++ src/main/java/xdb/GremlinDB.java
@@ -16,14 +16,29 @@ public class GremlinDB {
   private static Logger log = LogManager.getLogger(GremlinDB.class);
   private static Gson gson = new Gson();
 
-  interface Element {}
+  interface Element {
+    long uid();
+  }
+
   public static class Vertex implements Element {
-    public long uid;
+    public long uid = -1;
     public Map<String, Object> props;
+
+    public Vertex() {
+      this.uid = -1;
+      this.props = new HashMap<String, Object>();
+    }
+
+    public Vertex(long uid, Map<String, Object> props) {
+      this.uid = uid;
+      this.props = props;
+    }
 
     public Edge link(Vertex end, Map<String, Object> props) {
       return new Edge(this, end, props);
     }
+
+    public long uid() { return uid; }
 
     public String toString() {
       return "vertex:" + uid + "props:" + gson.toJson(props);
@@ -37,10 +52,17 @@ public class GremlinDB {
     public Vertex end;
 
     public Edge(Vertex start, Vertex end, Map<String, Object> props) {
+      this(-1, start, end, props);
+    }
+
+    public Edge(long uid, Vertex start, Vertex end, Map<String, Object> props) {
+      this.uid = uid;
       this.start = start;
       this.end = end;
       this.props = props;
     }
+
+    public long uid() { return uid; }
 
     public String toString() {
       return "edge:" + uid + "props:" + gson.toJson(props) + " start:" + start + " end:" + end;
@@ -51,6 +73,8 @@ public class GremlinDB {
   private String db;
   private final  String dbconfig = "create,cache_size=1GB,eviction=(threads_max=2,threads_min=2),lsm_manager=(merge=true,worker_thread_max=3), checkpoint=(log_size=2GB,wait=3600)";
 
+  private final Integer VERTEX_KIND = 0;
+  private final Integer EDGE_KIND = 1;
   private Connection conn;
   private Session session;
   private Cursor uids;
@@ -72,7 +96,7 @@ public class GremlinDB {
   public GremlinDB(String db) {
     this.db = db;
     init();
-  }  
+  }
 
   private void init() {
     checkDir(db);
@@ -80,7 +104,7 @@ public class GremlinDB {
     session = conn.open_session(null);
     session.create("table:uids", "key_format=r,value_format=u");
     uids = session.open_cursor("table:uids", null, "append");
-    session.create("table:tuples", "key_format=QS,value_format=S,columns=(uid,key,value)");
+    session.create("table:tuples", "key_format=qS,value_format=S,columns=(uid,key,value)");
     tuples = session.open_cursor("table:tuples", null, null);
     session.create("index:tuples:index", "columns=(key,value)");
     reversed = session.open_cursor("index:tuples:index(uid)", null, null);
@@ -89,13 +113,17 @@ public class GremlinDB {
   private long next_uid() {
     uids.putValueByteArray(new byte[]{(byte)0});
     uids.insert();
+    uids.next();
     return uids.getKeyRecord();
   }
 
   private void delete(long uid) {
-    uids.putKeyRecord(uid);
-    uids.search();
-    uids.remove();
+    log.info("delete");
+     uids.putKeyRecord(uid);
+    if(uids.search()==0) {
+      uids.putKeyRecord(uids.getKeyRecord());
+      uids.remove();
+    }
     uids.reset();
 
     tuples.putKeyLong(uid);
@@ -118,13 +146,14 @@ public class GremlinDB {
         } {
           tuples.reset();
           break;
-        }        
+        }
       }
       break;
     }
   }
 
   private void update(long uid, Map<String, Object> props) {
+    log.info("update");
     delete(uid);
     props.forEach((k,v) -> {
         tuples.putKeyLong(uid);
@@ -151,7 +180,7 @@ public class GremlinDB {
       } while(reversed.next()==0);
     }
     reversed.reset();
-    return ret;    
+    return ret;
   }
 
   private Object key(long uid, String key) {
@@ -159,18 +188,72 @@ public class GremlinDB {
     tuples.putKeyLong(uid);
     tuples.putKeyString(key);
     if(tuples.search() == 0) {
-      ret = gson.fromJson(tuples.getValueString(), new HashMap<String,Object>().getClass());
+      ret = getObj(tuples.getValueString());
     }
     tuples.reset();
     return ret;
   }
 
-  public Element get(long uid) {
-    return null;
+  private Object getObj(String val) {
+    Object ret = null;
+    if(val.startsWith("{")) {
+      ret = gson.fromJson(val, new HashMap<String,Object>().getClass());
+    } else
+      ret = Integer.parseInt(val);
+    return ret;
   }
 
-  public void save(Element element) {
+  public Element get(long uid) {
+    Element ret = null;
+    tuples.putKeyLong(uid);
+    SearchStatus st = tuples.search_near();
+    if (st != SearchStatus.NOTFOUND) {
+      if(st == SearchStatus.SMALLER) {
+        if(tuples.next() != 0)
+          return ret;
+      }
+      Map<String, Object> props = new HashMap<String, Object>();
+      do {
+        long tid = tuples.getKeyLong();
+        String key = tuples.getKeyString();
+        if(tid == uid) {
+          props.put(key, getObj(tuples.getValueString()));
+        } else
+          break;
+      } while(tuples.next() == 0);
+      Integer kind = (Integer)props.get("__kind__");
+      if(kind.equals(VERTEX_KIND)) {
+        ret = new Vertex(uid, props);
+      } else {
+        long start = (Long)props.get("__start__");
+        long end = (Long)props.get("__end__");
+        ret = new Edge(uid, (Vertex)get(start), (Vertex)get(end), props);
+      }
+    }
+    tuples.reset();
+    return ret;
+  }
 
+  public Element save(Element element) {
+    log.info("save");
+    if(element instanceof Vertex) {
+      Vertex v = (Vertex)element;
+      long uid = v.uid==-1? next_uid() : v.uid;
+      v.uid = uid;
+      Map<String, Object> props = new HashMap<String, Object>(v.props);
+      props.put("__kind__", VERTEX_KIND);
+      update(v.uid, props);
+    } else {
+      Edge e = (Edge)element;
+      long uid = e.uid==-1? next_uid() : e.uid;
+      e.uid = uid;
+      Map<String, Object> props = new HashMap<String, Object>(e.props);
+      props.put("__kind__", EDGE_KIND);
+      props.put("__start__", e.start.uid == -1? save(e.start).uid() : e.start.uid);
+      props.put("__end__", e.end.uid == -1? save(e.end).uid() : e.end.uid);
+      update(e.uid, props);
+    }
+    return element;
   }
 
   public void close() {
@@ -178,5 +261,17 @@ public class GremlinDB {
     conn.close(null);
   }
 
-  
+  private static void test1() {
+    GremlinDB gdb = new GremlinDB("acme");
+    Vertex v1 = new Vertex();
+    v1 = (Vertex)gdb.save(v1);
+    Element e = gdb.get(v1.uid);
+    log.info("e {}", e);
+    gdb.close();
+  }
+
+  public static void main(String[] args) {
+    test1();
+  }
+
 }
